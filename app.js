1e5ab210bf2575f20f7a891e692897978459e9cf
Harshdeep Singh Pruthi <2802harsh@gmail.com>
2021-05-01 20:55:52 +0530

    pay rent

--- app.js
+++ app.js
@@ -181,7 +181,41 @@ app.post('/tenant/login', async (req, res) => {
 
 app.get('/tenant/dashboard', (req, res) => {
     if(req.session.loggedIn && req.session.userType == "Tenant") {
-        res.render('tenantDashboard', {user: req.session.user})
+        let tenantId = req.session.user.Id;
+        let query = `SELECT 
+                        A.*, B.BuildingName, B.Address 
+                    FROM 
+                        ApartmentTable AS A
+                    INNER JOIN
+                        BuildingTable AS B
+                    ON
+                        A.BuildingId = B.Id
+                    WHERE
+                        TenantId = ${tenantId}
+                    `;
+        con.query(query, function (err, result, fields) {
+            if (err) throw err;
+            // console.log(result);
+            res.render('tenantDashboard', {user: req.session.user, rentedaps: result});
+        });
+    }
+    else
+        res.redirect("/tenant/login");
+})
+
+
+app.post('/tenant/payRent', (req,res) => {
+    if(req.session.loggedIn && req.session.userType == "Tenant") {
+        let buildingId = req.body.buildingId;
+        let apartmentId = req.body.apartmentId;
+        let tenantId = req.body.tenantId;
+        let rent = req.body.rent;
+        let date = new Date().toISOString().split("T")[0];
+        let query = `INSERT INTO PaymentTable (Date, BuildingId, ApartmentId, TenantId, PaidRent) VALUES ("${date}", "${buildingId}", "${apartmentId}", "${tenantId}", "${rent}");`
+        con.query(query, function (err, result, fields) {
+            if (err) throw err;
+            res.redirect('/tenant/dashboard');
+        });
     }
     else
         res.redirect("/tenant/login");
