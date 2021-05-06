4960119062bc69f026dd8ec9f4e8c2dab07a7ce1
sarthakm21 <sarthakmaheshwari140@gmail.com>
2021-05-07 00:43:29 +0530

    edit and add apartment styles

--- controllers/ownerController.js
+++ controllers/ownerController.js
@@ -12,7 +12,8 @@ let ownerRegisterPost = async (req, res) => {
   let contact = req.body.contact;
   let address = req.body.address;
   let password = req.body.password;
-  if(!(name && contact && address && password)) return res.redirect("/owner/register");
+  if (!(name && contact && address && password))
+    return res.redirect("/owner/register");
   let passwordHash = await bcrypt.hash(password, 10);
   let query = `INSERT INTO OwnerTable (OwnerName, OwnerContact, OwnerAddress, OwnerPassword) VALUES ("${name}", "${contact}", "${address}", "${passwordHash}");`;
   con.query(query, function (err, result) {
@@ -31,7 +32,7 @@ let ownerLoginGet = (req, res) => {
 let ownerLoginPost = async (req, res) => {
   let contact = req.body.contact;
   let password = req.body.password;
-  if(!(contact && password)) return res.redirect("/owner/login");
+  if (!(contact && password)) return res.redirect("/owner/login");
   let query = `SELECT * FROM OwnerTable WHERE OwnerContact = ${contact};`;
   con.query(query, function (err, result, fields) {
     if (err) {
@@ -75,14 +76,14 @@ let ownerDashboard = (req, res) => {
                     ON B.Id = A.BuildingId
                     WHERE B.OwnerId = ${req.session.user.Id}
                     ORDER BY PaymentDate DESC
-                    LIMIT 5`
-        con.query(query2, function(err, resu){
-            res.render("ownerDashboard", {
-                    user: req.session.user,
-                    buildings: result,
-                    recentPayments: resu
-                  });
-        })
+                    LIMIT 5`;
+      con.query(query2, function (err, resu) {
+        res.render("ownerDashboard", {
+          user: req.session.user,
+          buildings: result,
+          recentPayments: resu,
+        });
+      });
     });
   } else res.redirect("/owner/login");
 };
@@ -138,35 +139,34 @@ let ownerBuildingPost = async (req, res) => {
 };
 
 let ownerBuildingPut = async (req, res) => {
-    if(req.session.loggedIn && req.session.userType == "Owner") {
-        let buildingId = req.params.id;
-        let buildingName = req.body.name;
-        let address = req.body.address;
-        let floors = req.body.floors;
-        let query = `UPDATE 
+  if (req.session.loggedIn && req.session.userType == "Owner") {
+    let buildingId = req.params.id;
+    let buildingName = req.body.name;
+    let address = req.body.address;
+    let floors = req.body.floors;
+    let query = `UPDATE 
                         BuildingTable
                     SET 
                         BuildingName = "${buildingName}", Address = "${address}", Floors = "${floors}"
                     WHERE
                         Id = ${buildingId};
-                    `
-        con.query(query, function (err, result) {
-            if (err) {
-              console.log(err);
-              return res.redirect("/owner/dashboard");
-            }
-            res.redirect("/owner/dashboard");
-        });
-    }
-    else {
-        res.redirect("/owner/login");
-    }
-}
+                    `;
+    con.query(query, function (err, result) {
+      if (err) {
+        console.log(err);
+        return res.redirect("/owner/dashboard");
+      }
+      res.redirect("/owner/dashboard");
+    });
+  } else {
+    res.redirect("/owner/login");
+  }
+};
 
 let ownerApartmentsGet = async (req, res) => {
-    if(req.session.loggedIn && req.session.userType == "Owner") {
-        let buildingId = req.params.buildingid;
-        let query = `SELECT
+  if (req.session.loggedIn && req.session.userType == "Owner") {
+    let buildingId = req.params.buildingid;
+    let query = `SELECT
                         A.Id AS ApartmentId, A.FlatNumber, A.Rent, A.StartDate, A.TenantId, A.LastPaymentDate, T.TenantName
                     FROM
                         (SELECT
@@ -183,81 +183,83 @@ let ownerApartmentsGet = async (req, res) => {
                         A.TenantId = T.Id
                     WHERE
                         A.BuildingId = ${buildingId}
-                    `
-        con.query(query, function (err, result) {
-            if (err) {
-              throw err;
-            }
-            let query2 = `SELECT 
+                    `;
+    con.query(query, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      let query2 = `SELECT 
                             * 
-                          FROM TenantTable`
-            con.query(query2, function (error, result2){
-                let query3 = `SELECT 
+                          FROM TenantTable`;
+      con.query(query2, function (error, result2) {
+        let query3 = `SELECT 
                                 * 
                             FROM 
                                 BuildingTable
                             WHERE
-                                Id = ${buildingId}`
-                con.query(query3, function (error, result3){
-                    // res.send({result,result2,result3});
-                    res.render('ownerApartments', {user: req.session.user, apartments: result, tenants: result2, building: result3});
-                })
-            });
+                                Id = ${buildingId}`;
+        con.query(query3, function (error, result3) {
+          // res.send({result,result2,result3});
+          res.render("ownerApartments", {
+            user: req.session.user,
+            apartments: result,
+            tenants: result2,
+            building: result3,
+          });
         });
-    }
-    else{
-        res.redirect("/owner/login");
-    }
-}
+      });
+    });
+  } else {
+    res.redirect("/owner/login");
+  }
+};
 
 let ownerApartmentPut = async (req, res) => {
-    if(req.session.loggedIn && req.session.userType == "Owner") {
-        let buildingId = req.params.buildingid;
-        let apartmentId = req.body.apartmentId || null;
-        let tenantId = req.body.tenantId;
-        let flatNumber = req.body.flatNumber;
-        let rent = req.body.rent;
-        let startDate = req.body.startDate;
-        let query ;
-        if(apartmentId === null){
-            query= `
+  if (req.session.loggedIn && req.session.userType == "Owner") {
+    let buildingId = req.params.buildingid;
+    let apartmentId = req.body.apartmentId || null;
+    let tenantId = req.body.tenantId;
+    let flatNumber = req.body.flatNumber;
+    let rent = req.body.rent;
+    let startDate = req.body.startDate;
+    let query;
+    if (apartmentId === null) {
+      query = `
                     INSERT INTO
                         ApartmentTable (BuildingId, FlatNumber, TenantId, Rent, StartDate)
                     VALUES
                         ( "${buildingId}", "${flatNumber}", "${tenantId}", "${rent}", "${startDate}")
-                    `
-        }
-        else{
-            query = `UPDATE
+                    `;
+    } else {
+      query = `UPDATE
                         ApartmentTable
                     SET 
                         FlatNumber = "${flatNumber}", TenantId = "${tenantId}", Rent = "${rent}", StartDate = "${startDate}"
                     WHERE
-                        Id = ${apartmentId}`
-        }
-        console.log(query);
-        con.query(query, function (err, result) {
-            if (err) {
-              console.log(err);
-              return res.redirect("/owner/dashboard");
-            }
-            res.redirect(`/owner/buildings/${buildingId}`);
-        });
+                        Id = ${apartmentId}`;
     }
-    else{
-        res.redirect("/owner/login");
-    }
-}
+    console.log(query);
+    con.query(query, function (err, result) {
+      if (err) {
+        console.log(err);
+        return res.redirect("/owner/dashboard");
+      }
+      res.redirect(`/owner/buildings/${buildingId}`);
+    });
+  } else {
+    res.redirect("/owner/login");
+  }
+};
 
 module.exports = {
-    ownerBuildingGet,
-    ownerBuildingPost,
-    ownerBuildingPut,
-    ownerApartmentsGet,
-    ownerApartmentPut,
-    ownerDashboard,
-    ownerLoginGet,
-    ownerLoginPost,
-    ownerRegisterGet,
-    ownerRegisterPost
-}
+  ownerBuildingGet,
+  ownerBuildingPost,
+  ownerBuildingPut,
+  ownerApartmentsGet,
+  ownerApartmentPut,
+  ownerDashboard,
+  ownerLoginGet,
+  ownerLoginPost,
+  ownerRegisterGet,
+  ownerRegisterPost,
+};
